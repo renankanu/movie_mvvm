6d08b8263eaf2aa59471b36f39c14f271be33705
renankanu <renankanu@gmail.com>
2024-05-30 11:13:08 -0300

    refactor: change repository to functional programming

--- lib/repository/movie_repository.dart
+++ lib/repository/movie_repository.dart
@@ -1,5 +1,8 @@
+import 'package:fpdart/fpdart.dart';
+
+import '../core/errors/errors.dart';
 import '../model/movie.dart';
 
 abstract class MovieRepository {
-  Future<List<Movie>> getTrendTodayMovies();
+  Future<Either<Failure, List<Movie>>> getTrendTodayMovies();
 }
--- lib/repository/movie_repository_impl.dart
+++ lib/repository/movie_repository_impl.dart
@@ -1,9 +1,11 @@
+import 'package:fpdart/fpdart.dart';
+import 'package:movie_mvvm/core/errors/errors.dart';
 import 'package:movie_mvvm/model/movie.dart';
 import 'package:movie_mvvm/repository/movie_repository.dart';
 
 class MovieRepositoryImpl implements MovieRepository {
   @override
-  Future<List<Movie>> getTrendTodayMovies() async {
+  Future<Either<Failure, List<Movie>>> getTrendTodayMovies() async {
     throw UnimplementedError();
   }
 }
